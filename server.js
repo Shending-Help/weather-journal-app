b74147cf28d100840f156b51faa405e9bf5fbc5d
Youssef <yusufcheetos99@gmail.com>
2020-12-12 22:59:51 +0200

    adding dependices and setting the server up

--- server.js
+++ server.js
@@ -2,18 +2,49 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser')
+
+
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+const { response } = require('express');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+app.listen(port, listening);
+
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: ${port}`);
+}
+
+app.get('/getAll', (req, res) => {
+    res.send(projectData).status(200).end();
+})
+app.post('/postData', addWeather );
+
+function addWeather (req){
+    let data = req.body
+    
+    projectData.push({
+        temp : data.temp ,
+        date : data.date , 
+        feeling : data.feeling
+
+    });
+    res.send(projectData).status(404).end();
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,67 @@
 /* Global Variables */
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=5eff5063dfc1d10eb8fd453b13548c61&units=imperial' ;
+const apiUrl = "http://localhost:3000/";
+
+// variables for elements to be used
+const zipCodeEl = document.getElementById('zip');
+const feelingsCodeEl = document.getElementById('feelings');
+const dateEl = document.getElementById('date');
+const tempEl = document.getElementById('temp');
+const contentEl = document.getElementById('content');
+
+const catchErr = (error) => console.error('an Erorr occured ', error);
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+const zipCode =  zipCodeEl.value;
+
+function performAction(){
+//  let url = baseUrl + zipCode + '&appid=' + apiKey ;
+
+    
+    //Post Data To Api For Get Zip Code Information
+    getWeatherData(zipCode).then(zipInfo => {
+        //Return And Show Alert If City Is Not Found
+        if (zipInfo.cod != 200)
+            return alert(zipInfo.message)
+
+        //Now Post Data To Server For Saving And Display In Holder Section
+        data.temp = zipInfo.list[0].main.temp;
+        postDateToServer(data);
+    }).catch(catchError);
+}
+
+async function getWeatherData(zipCode) {
+    return await (await fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}${apiKey}`)).json()
+}
+
+/** Post Data To Server For Saving  */
+async function postDateToServer(data) {
+    let response = await fetch(`${apiUrl}postData`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+    try {
+        if (!response.ok) {
+            alert('Process Not Successfuly');
+            return;
+        }
+       
+        response.json().then(data => {
+            if (response.ok)
+                updateUI();//Update UI Now
+            else
+                alert('Process Not Successfuly');
+        }).catch(catchError);
+
+    } catch (error) {
+        catchError(error);
+    }
+}
